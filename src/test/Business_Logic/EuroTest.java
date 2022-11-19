6a8e87e38e515793938a6866326361a49e5928ff
AlexPaladino <paladino.alessio391@gmail.com>
2022-11-19 15:37:04 +0100

    Added Before in EuroTest

--- src/test/Business_Logic/EuroTest.java
+++ src/test/Business_Logic/EuroTest.java
@@ -4,53 +4,41 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
-
 import code.Business_Logic.Euro;
 
 public class EuroTest {
-    @Test
-    public void testGetValore() {
-        Euro example = new Euro(42f);
-        assertEquals(4200, example.getValore());
+
+    Euro testEuro1, testEuro2;
+    
+    @Before
+    public void setUp() {
+        testEuro1 = new Euro(42);
+        testEuro2 = new Euro(43);
     }
 
     @Test
-    public void testMinoreDi() {
-        Euro ex1 = new Euro(42f);
-        Euro ex2 = new Euro(45f);
+    public void testGetValore() { assertEquals(4200, testEuro1.getValore()); }
 
-        assertTrue(ex1.minoreDi(ex2));
-    }
+    @Test
+    public void testMinoreDi() { assertTrue(testEuro1.minoreDi(testEuro2)); }
 
     @Test
     public void testSomma() {
-        Euro example = new Euro(42f);
-        example.somma(example);
-
-        assertEquals(example.getValore(), 8400);
+        testEuro1.somma(testEuro1);
+        assertEquals(testEuro1.getValore(), 8400);
     }
 
     @Test
     public void testSottrai() {
-        Euro example = new Euro(42f);
-        example.sottrai(example);
-
-        assertEquals(example.getValore(), 0);
+        testEuro1.sottrai(testEuro1);
+        assertEquals(testEuro1.getValore(), 0);
     }
 
     @Test
-    public void testStampa() {
-        Euro example = new Euro(42f);
-        
-        assertEquals(example.stampa(), "42.0 euro");
-    }
+    public void testStampa() { assertEquals(testEuro1.stampa(), "42.0 euro"); }
 
     @Test
-    public void testUgualeA() {
-        Euro ex1 = new Euro(42f);
-        Euro ex2 = new Euro(42.3f);
-
-        assertFalse(ex1.equals(ex2));
-    }
+    public void testUgualeA() { assertFalse(testEuro1.equals(testEuro2)); }
 }
